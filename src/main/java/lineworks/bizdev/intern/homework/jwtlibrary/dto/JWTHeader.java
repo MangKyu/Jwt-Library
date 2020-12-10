c8498d42f82b5df73da6afc622b72b5928d16bdb
MangKyu <whalsrb1226@naver.com>
2020-12-10 14:59:25 +0900

    Jwt->JWT

--- src/main/java/lineworks/bizdev/intern/homework/jwtlibrary/dto/JWTHeader.java
+++ src/main/java/lineworks/bizdev/intern/homework/jwtlibrary/dto/JWTHeader.java
@@ -3,11 +3,11 @@ package lineworks.bizdev.intern.homework.jwtlibrary.dto;
 // WORKS MOBILE 2020 하반기 Internship
 // 작성자 : 김동헌
 
-public class JwtHeader {
+public class JWTHeader {
     private String typ;
     private String alg;
 
-    public JwtHeader(String algorithm) {
+    public JWTHeader(String algorithm) {
         typ = "JWT";
         alg = algorithm;
     }
