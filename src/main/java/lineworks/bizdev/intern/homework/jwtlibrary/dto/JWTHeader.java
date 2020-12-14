a5fac79762a6e8883e69065544e42f668dc33861
MangKyu <whalsrb1226@naver.com>
2020-12-14 15:36:33 +0900

    JWTClaimSet 추가 및 그에 수반해서 JWTTokenizer.generateClaimSetToken() 와 유닛테스트 추가

--- src/main/java/lineworks/bizdev/intern/homework/jwtlibrary/dto/JWTHeader.java
+++ src/main/java/lineworks/bizdev/intern/homework/jwtlibrary/dto/JWTHeader.java
@@ -4,11 +4,11 @@ package lineworks.bizdev.intern.homework.jwtlibrary.dto;
 // 작성자 : 김동헌
 
 public class JWTHeader {
-    private String typ;
     private String alg;
+    private String typ;
 
     public JWTHeader(String algorithm) {
-        typ = "JWT";
         alg = algorithm;
+        typ = "JWT";
     }
 }
