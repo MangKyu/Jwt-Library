99f1204ba300ab78499d0bd5f09fdfd2b2d5a355
minkyu.jo <minkyu.jo@worksmobile.com>
2020-12-28 19:49:45 +0900

    토큰 발급을 위한 암호화 알고리즘, 상수 클래스 추가

---
+++ src/main/java/lineworks/bizdev/intern/homework/mylibs/jwt/constants/TokenConstants.java
@@ -0,0 +1,9 @@
+package lineworks.bizdev.intern.homework.mylibs.jwt.constants;
+
+public final class TokenConstants {
+
+	public static final String JWT_TOKEN_TYPE = "JWT";
+
+	public static final String CLAIMS_EXPIRED_AT = "exp";
+
+}
