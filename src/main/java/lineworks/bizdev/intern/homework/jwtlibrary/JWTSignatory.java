77cd19ec61daaec787d20f2fa6a05d580cc47f1d
MangKyu <whalsrb1226@naver.com>
2020-12-28 19:42:27 +0900

    Jwt 코드 재설계 및 기존 코드 삭제

--- src/main/java/lineworks/bizdev/intern/homework/jwtlibrary/JWTSignatory.java
+++
@@ -1,8 +0,0 @@
-package lineworks.bizdev.intern.homework.jwtlibrary;
-
-// WORKS MOBILE 2020 하반기 Internship
-// 작성자 : 김동헌
-
-public interface JWTSignatory {
-    public byte[] generateSign(String message) throws Exception;
-}
