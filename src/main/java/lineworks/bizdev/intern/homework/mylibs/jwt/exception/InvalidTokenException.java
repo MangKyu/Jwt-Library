79df5962b31b2c7a80ac5d16165791eaffdc87f5
minkyu.jo <minkyu.jo@worksmobile.com>
2020-12-28 19:43:08 +0900

    Jwt 토큰 예외 추가

---
+++ src/main/java/lineworks/bizdev/intern/homework/mylibs/jwt/exception/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package lineworks.bizdev.intern.homework.mylibs.jwt.exception;
+
+public class InvalidTokenException extends RuntimeException {
+
+	public InvalidTokenException(String message) {
+		super(message);
+	}
+
+}
