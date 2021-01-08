d97c10dad2ce8bc25ede1192cade6b898e0c438d
MangKyu <whalsrb1226@naver.com>
2021-01-08 17:55:33 +0900

    Component 재설계 및 불필요한 코드 삭제

--- src/main/java/lineworks/bizdev/intern/homework/mylibs/jwt/component/SignWith.java
+++ src/main/java/lineworks/bizdev/intern/homework/mylibs/jwt/component/SignWith.java
@@ -1,65 +1,21 @@
 package lineworks.bizdev.intern.homework.mylibs.jwt.component;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-
-import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
 
 import lineworks.bizdev.intern.homework.mylibs.jwt.constants.EncryptAlgorithm;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 
 @Getter
-@RequiredArgsConstructor
-@Log4j2
 public class SignWith {
 
 	private final EncryptAlgorithm encryptAlgorithm;
-	private final Object key;
-	private final boolean rsa;
+	private final Key key;
 
 	@Builder
-	public SignWith(EncryptAlgorithm encryptAlgorithm, Object key, String keyPath) throws
-		NoSuchAlgorithmException, InvalidKeySpecException {
+	public SignWith(EncryptAlgorithm encryptAlgorithm, Key key) {
 		this.encryptAlgorithm = encryptAlgorithm;
-		this.key = keyPath == null ? key : readPrivateKey(keyPath);
-		rsa = encryptAlgorithm.isRsa();
-	}
-
-	private PrivateKey readPrivateKey(String keyPath) throws NoSuchAlgorithmException, InvalidKeySpecException {
-		PrivateKey key = null;
-
-		File file = new File(keyPath);
-		try (
-			FileInputStream fis = new FileInputStream(file);
-			DataInputStream dis = new DataInputStream(fis);
-		) {
-			byte[] keyBytes = new byte[(int)file.length()];
-			dis.readFully(keyBytes);
-
-			String temp = new String(keyBytes);
-			String privKeyPEM = temp.replace("-----BEGIN PRIVATE KEY-----", "");
-			privKeyPEM = privKeyPEM.replace("-----END PRIVATE KEY-----", "");
-
-			byte[] decoded = DatatypeConverter.parseBase64Binary(privKeyPEM);
-
-			PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(decoded);
-			KeyFactory kf = KeyFactory.getInstance("RSA");
-			key = kf.generatePrivate(spec);
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		}
-
-		return key;
+		this.key = key;
 	}
 
 }
