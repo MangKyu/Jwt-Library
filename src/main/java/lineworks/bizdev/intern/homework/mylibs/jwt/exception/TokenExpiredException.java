82b9d6a872d415ddfee02c8bd2905e6b8d8adb41
MangKyu <whalsrb1226@naver.com>
2021-01-08 17:55:00 +0900

    사용하지 않을 클래스 삭제

--- src/main/java/lineworks/bizdev/intern/homework/mylibs/jwt/exception/TokenExpiredException.java
+++
@@ -1,9 +0,0 @@
-package lineworks.bizdev.intern.homework.mylibs.jwt.exception;
-
-public class TokenExpiredException extends RuntimeException {
-
-	public TokenExpiredException(String message) {
-		super("This Token is Expired: " + message);
-	}
-
-}
