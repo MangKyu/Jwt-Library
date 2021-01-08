c0eeafa81013423046b1ff3d9ffc528d8a91e433
minkyu.jo <minkyu.jo@worksmobile.com>
2021-01-08 17:55:00 +0900

    사용하지 않을 클래스 삭제

--- src/main/java/lineworks/bizdev/intern/homework/mylibs/jwt/exception/InvalidTokenException.java
+++
@@ -1,9 +0,0 @@
-package lineworks.bizdev.intern.homework.mylibs.jwt.exception;
-
-public class InvalidTokenException extends RuntimeException {
-
-	public InvalidTokenException(String message) {
-		super("This Token is Invalid" + message);
-	}
-
-}
